9688eef939783fe8a8fa4706a1dce61bf1c69154
Daniel P. Wright <dani@dpwright.com>
2012-10-04 18:07:49 +0900

    Site updated at 2012-10-04 09:07:49 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,9 +1,11 @@
+// -*- coding:utf-8 -*-
 var github = (function(){
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+(repos[i].description||'')+'</p></li>';
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a> '+
+        repos[i].description+'</li>';
     }
     t.innerHTML = fragment;
   }
@@ -12,7 +14,10 @@ var github = (function(){
       $.ajax({
           url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
         , type: 'jsonp'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
+        , error: function (err) {
+          $(options.target + ' li.loading').addClass('error')
+            .text("読み込み失敗");
+        }
         , success: function(data) {
           var repos = [];
           if (!data || !data.data) { return; }
