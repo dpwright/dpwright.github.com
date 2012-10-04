4e209d15f4772d43ecd8606d29bd6924e0462e45
Daniel P. Wright <dani@dpwright.com>
2012-10-04 18:04:13 +0900

    Install kui theme

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -4,7 +4,7 @@ var github = (function(){
     var i = 0, fragment = '', t = $(target)[0];
 
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a> : '+
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a> '+
         repos[i].description+'</li>';
     }
     t.innerHTML = fragment;
@@ -12,11 +12,7 @@ var github = (function(){
   return {
     showRepos: function(options){
       $.ajax({
-        url: "https://api.github.com/users/"+options.user+"/repos?"+
-          "type="+options.type+
-          "&sort="+options.sort+
-          "&direction="+options.direction+
-          "&callback=?"
+          url: "https://api.github.com/users/"+options.user+"/repos?callback=?"
         , type: 'jsonp'
         , error: function (err) {
           $(options.target + ' li.loading').addClass('error')
@@ -25,18 +21,19 @@ var github = (function(){
         , success: function(data) {
           var repos = [];
           if (!data || !data.data) { return; }
-          for (var i = 0; i < options.count; i++) {
+          for (var i = 0; i < data.data.length; i++) {
             if (options.skip_forks && data.data[i].fork) { continue; }
             repos.push(data.data[i]);
           }
-          // repos.sort(function(a, b) {
-          //   var aDate = new Date(a.pushed_at).valueOf(),
-          //   bDate = new Date(b.pushed_at).valueOf();
+          repos.sort(function(a, b) {
+            var aDate = new Date(a.pushed_at).valueOf(),
+                bDate = new Date(b.pushed_at).valueOf();
 
-          //   if (aDate === bDate) { return 0; }
-          //   return aDate > bDate ? -1 : 1;
-          // });
+            if (aDate === bDate) { return 0; }
+            return aDate > bDate ? -1 : 1;
+          });
 
+          if (options.count) { repos.splice(options.count); }
           render(options.target, repos);
         }
       });
