47f92d416eee4105bf29f178a01a5c09afda8c5e
Daniel P. Wright <dani@dpwright.com>
2015-03-17 07:12:40 +0000

    Snapshot 03/17/15 07:12

---
+++ posts/2015/03/12/getting-up-and-running-with-gl/fragmentShader.glsl
@@ -0,0 +1,38 @@
+#version 330
+
+uniform vec4 diffuseColour;
+uniform vec4 ambientColour;
+uniform vec4 specularColour;
+uniform float shininess;
+
+uniform vec3 lightDirection;
+
+uniform sampler2D diffuseMap;
+
+in vec3 calculatedNormal;
+in vec4 calculatedEye;
+in vec4 rgba;
+in vec2 fragmentUV;
+
+out vec4 colorOut;
+
+void main()
+{
+    vec4 spec = vec4(0.0);
+
+    vec3 n = normalize(calculatedNormal);
+    vec3 e = normalize(vec3(calculatedEye));
+
+    float intensity = max(dot(n,lightDirection), 0.0);
+
+    if (intensity > 0.0)
+    {
+        vec3 h = normalize(lightDirection + e);
+
+        float intSpec = max(dot(h,n), 0.0);
+        spec = specularColour * pow(intSpec,shininess);
+    }
+
+    vec4 texCol = texture(diffuseMap, fragmentUV);
+    colorOut = texCol * rgba * max(intensity * diffuseColour + spec, ambientColour);
+}
